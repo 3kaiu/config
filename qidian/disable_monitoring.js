c39d54b93b7b949096db825d7cca7de21548a030
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-22 23:42:13 +0800

    Add files via upload

---
+++ qidian/disable_monitoring.js
@@ -0,0 +1,12 @@
+// disable_monitoring.js
+
+let body = JSON.parse($response.body);
+
+// 遍历 features，将所有 enabled 设为 false，sample_ratio 设为 0
+body.data.features.forEach((feature) => {
+  feature.enabled = false; // 关闭监控
+  feature.sample_ratio = 0; // 设置采样率为0
+});
+
+// 返回修改后的 body
+$done({ body: JSON.stringify(body) });
---
+++ qidian/init.js
@@ -0,0 +1,22 @@
+let body = JSON.parse($response.body);
+
+// 遍历 features，将所有 enabled 设为 false，sample_ratio 设为 0
+body.Data.PushDialogFrequency = Number.MAX_SAFE_INTEGER;
+body.Data.FreshmanCarryOnPlatSwitch = 0;
+body.Data.NotificationPageBackPushNoticeFrequency = "360";
+body.Data.EnableMonitorLog = 0;
+body.Data.Freshman.WelfareTab = 1;
+body.Data.UGCPushNoticeFrequency = "360";
+body.Data.PushDialogScenes = [];
+body.Data.PushNoticeFrequency = 0;
+body.Data.GDT.Account.Enable = 1;
+body.Data.GDT.Popup.Enable = 1;
+body.Data.ActivityPageBackPushNoticeFrequency = "360";
+body.Data.Member.IsMember = 1;
+body.Data.Member.MemberType = 1;
+body.Data.CheckInCaseSub = "1";
+body.Data.EnablePresent = 1;
+body.Data.IsFreeReadingUser = true;
+
+// 返回修改后的 body
+$done({ body: JSON.stringify(body) });
