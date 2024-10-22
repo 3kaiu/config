0b90da01defaa6c470c1688d127c3cb77a7a4905
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-23 00:22:59 +0800

    Update disable_monitoring.js

--- qidian/disable_monitoring.js
+++ qidian/disable_monitoring.js
@@ -3,7 +3,7 @@
 let body = JSON.parse($response.body);
 
 // 遍历 features，将所有 enabled 设为 false，sample_ratio 设为 0
-body.data.features.forEach((feature) => {
+body.Data.features.forEach((feature) => {
   feature.enabled = false; // 关闭监控
   feature.sample_ratio = 0; // 设置采样率为0
 });
