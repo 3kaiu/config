e43a9ccd4880cbab8d3266945135a1261e5d09ba
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-23 00:35:28 +0800

    Update filter2.js

--- qidian/filter2.js
+++ qidian/filter2.js
@@ -1,6 +1,9 @@
 let body = JSON.parse($response.body);
 
-body.Data.BenefitButtonList = body.Data.BenefitButtonList[0];
+body.Data.BenefitButtonList = [body.Data.BenefitButtonList[0]];
+body.Data.FunctionButtonList = [];
+body.Data.BottomButtonList = [];
+body.Data.Member = {};
 
 // 返回修改后的 body
 $done({ body: JSON.stringify(body) });
