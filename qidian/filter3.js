4ab2e76f10b07d9fef2cfef76414cc6101f8f47c
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-23 20:55:55 +0800

    Update filter3.js

--- qidian/filter3.js
+++ qidian/filter3.js
@@ -1,12 +1,16 @@
 let body = JSON.parse($response.body);
 
+// 清空 EntranceTabItems 数组
 body.Data.EntranceTabItems = [];
+
+// 确保从 CountdownBenefitModule 中提取 TaskList 的正确元素
 body.Data.CountdownBenefitModule.TaskList = [
   body.Data.CountdownBenefitModule.TaskList[0],
-  bodyData.CountdownBenefitModule.TaskList[1],
-];
-body.Data.MonthBenefitModule = {};
-body.Data.BaizeModule = {};
+  body.Data.CountdownBenefitModule.TaskList[1]];
+
+// 清空 MonthBenefitModule 和 BaizeModule
+// body.Data.MonthBenefitModule = {};
+// body.Data.BaizeModule = {};
 
 // 返回修改后的 body
 $done({ body: JSON.stringify(body) });
