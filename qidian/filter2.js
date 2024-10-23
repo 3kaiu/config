845e58a087a6defd344dce77e7f03114f045b6e3
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-23 20:15:38 +0800

    Update filter2.js

--- qidian/filter2.js
+++ qidian/filter2.js
@@ -4,6 +4,9 @@ body.Data.BenefitButtonList = [body.Data.BenefitButtonList[0]];
 body.Data.FunctionButtonList = [];
 body.Data.BottomButtonList = [];
 body.Data.Member = {};
+body.Data.SchoolText = "";
+body.Data.SchoolUrl = "";
+body.Data.AccountBalance.QdFreeBalance = 10000;
 
 // 返回修改后的 body
 $done({ body: JSON.stringify(body) });
