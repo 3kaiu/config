09d7ffef19ea79693c0fbc6d951d7630d0fbb118
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-23 21:08:06 +0800

    Update filter2.js

--- qidian/filter2.js
+++ qidian/filter2.js
@@ -1,12 +1,19 @@
 let body = JSON.parse($response.body);
 
 body.Data.BenefitButtonList = [body.Data.BenefitButtonList[0]];
+body.Data.Data.DianNiangWorldSwitch = 0;
 body.Data.FunctionButtonList = [];
 body.Data.BottomButtonList = [];
 body.Data.Member = {};
 body.Data.SchoolText = "";
 body.Data.SchoolUrl = "";
+body.Data.SchoolImage = '';
 body.Data.AccountBalance.QdFreeBalance = 10000;
+body.Data.AccountBalance.QdBalance = 10002;
+body.Data.AccountBalance.YdFreeBalance = 10001;
+body.Data.AccountBalance.YdBalance = 10003;
+body.Data.AccountBalance.QdWorthBalance = 10004;
+
 
 // 返回修改后的 body
 $done({ body: JSON.stringify(body) });
