4e0bf683698603ac8ddf929ce14ae9469694b5ff
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-24 23:49:22 +0800

    Update filter3.js

--- qidian/filter3.js
+++ qidian/filter3.js
@@ -10,9 +10,8 @@ body.Data.CountdownBenefitModule.TaskList = [
   body.Data.CountdownBenefitModule.TaskList[0],
   body.Data.CountdownBenefitModule.TaskList[1]];
 
-// 清空 MonthBenefitModule 和 BaizeModule
-// body.Data.MonthBenefitModule = {};
-// body.Data.BaizeModule = {};
+body.Data.MonthBenefitModule = {};
+body.Data.BaizeModule = {};
 
 // 返回修改后的 body
 $done({ body: JSON.stringify(body) });
