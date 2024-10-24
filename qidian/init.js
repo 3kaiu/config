f2e6e5b3d3e5c1e615d5b9d7cdea3c04c79a6f61
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-24 23:51:35 +0800

    Update init.js

--- qidian/init.js
+++ qidian/init.js
@@ -1,21 +1,28 @@
-let body = JSON.parse($response.body);
+if ($response) {
+  const data = $response.body;
+  console.log("🚀🚀->>>>>>>>>>> init初始化2");
+  try {
+    const body = JSON.parse(data);
+    body.Data.PushDialogFrequency = Number.MAX_SAFE_INTEGER;
+    body.Data.FreshmanCarryOnPlatSwitch = 0;
+    body.Data.NotificationPageBackPushNoticeFrequency = "360";
+    body.Data.EnableMonitorLog = 0;
+    body.Data.UGCPushNoticeFrequency = "360";
+    body.Data.PushDialogScenes = [];
+    body.Data.PushNoticeFrequency = 0;
+    body.Data.GDT.Account.Enable = 1;
+    body.Data.GDT.Popup.Enable = 1;
+    body.Data.ActivityPageBackPushNoticeFrequency = "360";
+    body.Data.Member.IsMember = 1;
+    body.Data.Member.MemberType = 1;
+    body.Data.CheckInCaseSub = "1";
+    body.Data.EnablePresent = 1;
+    body.Data.IsFreeReadingUser = true;
 
-// 遍历 features，将所有 enabled 设为 false，sample_ratio 设为 0
-body.Data.PushDialogFrequency = Number.MAX_SAFE_INTEGER;
-body.Data.FreshmanCarryOnPlatSwitch = 0;
-body.Data.NotificationPageBackPushNoticeFrequency = "360";
-body.Data.EnableMonitorLog = 0;
-body.Data.UGCPushNoticeFrequency = "360";
-body.Data.PushDialogScenes = [];
-body.Data.PushNoticeFrequency = 0;
-body.Data.GDT.Account.Enable = 1;
-body.Data.GDT.Popup.Enable = 1;
-body.Data.ActivityPageBackPushNoticeFrequency = "360";
-body.Data.Member.IsMember = 1;
-body.Data.Member.MemberType = 1;
-body.Data.CheckInCaseSub = "1";
-body.Data.EnablePresent = 1;
-body.Data.IsFreeReadingUser = true;
-
-// 返回修改后的 body
-$done({ body: JSON.stringify(body) });
+    $done({ body: JSON.stringify(body) });
+  } catch (error) {
+    console.log("JSON Parse error:", error);
+  }
+} else {
+  console.error("Response is undefined⚠️警告->>>>>>>>>>>>>>>>");
+}
