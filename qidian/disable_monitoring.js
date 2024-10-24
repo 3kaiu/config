f9b9cbbc80ffe292cd45ea7900fd8d25fc0e53ff
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-24 23:58:03 +0800

    Update disable_monitoring.js

--- qidian/disable_monitoring.js
+++ qidian/disable_monitoring.js
@@ -1,12 +1,17 @@
-// disable_monitoring.js
+if ($response) {
+  const data = $response.body;
+  console.log("🚀🚀->>>>>>>>>>> disable初始化");
+  try {
+    const body = JSON.parse(data);
+    body.Data.features.forEach((feature) => {
+      feature.enabled = false; // 关闭监控
+      feature.sample_ratio = 0; // 设置采样率为0
+    });
 
-let body = JSON.parse($response.body);
-
-// 遍历 features，将所有 enabled 设为 false，sample_ratio 设为 0
-body.Data.features.forEach((feature) => {
-  feature.enabled = false; // 关闭监控
-  feature.sample_ratio = 0; // 设置采样率为0
-});
-
-// 返回修改后的 body
-$done({ body: JSON.stringify(body) });
+    $done({ body: JSON.stringify(body) });
+  } catch (error) {
+    console.log("JSON Parse error:", error);
+  }
+} else {
+  console.error("Response is undefined⚠️警告->>>>>>>>>>>>>>>>");
+}
