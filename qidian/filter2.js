698f1a040941e116348fee7f7a59f4b86d92d710
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-24 22:34:01 +0800

    Update filter2.js

--- qidian/filter2.js
+++ qidian/filter2.js
@@ -1,22 +1,24 @@
-let body = JSON.parse($response.body);
+if ($response) {
+  try {
+    const body = JSON.parse($response.body);
+    body.Data.BenefitButtonList = [body.Data.BenefitButtonList[0]];
+    body.Data.Data.DianNiangWorldSwitch = 0;
+    body.Data.FunctionButtonList = [];
+    body.Data.BottomButtonList = [];
+    body.Data.Member = {};
+    body.Data.SchoolText = "";
+    body.Data.SchoolUrl = "";
+    body.Data.SchoolImage = "";
+    body.Data.AccountBalance.QdFreeBalance = 10000;
+    body.Data.AccountBalance.QdBalance = 10002;
+    body.Data.AccountBalance.YdFreeBalance = 10001;
+    body.Data.AccountBalance.YdBalance = 10003;
+    body.Data.AccountBalance.QdWorthBalance = 10004;
 
-console.log('匹配到了，输出1-$response---->',$response)
-console.log('匹配到了，输出2---$request-->',$request)
-
-body.Data.BenefitButtonList = [body.Data.BenefitButtonList[0]];
-body.Data.Data.DianNiangWorldSwitch = 0;
-body.Data.FunctionButtonList = [];
-body.Data.BottomButtonList = [];
-body.Data.Member = {};
-body.Data.SchoolText = "";
-body.Data.SchoolUrl = "";
-body.Data.SchoolImage = '';
-body.Data.AccountBalance.QdFreeBalance = 10000;
-body.Data.AccountBalance.QdBalance = 10002;
-body.Data.AccountBalance.YdFreeBalance = 10001;
-body.Data.AccountBalance.YdBalance = 10003;
-body.Data.AccountBalance.QdWorthBalance = 10004;
-
-
-// 返回修改后的 body
-$done({ body: JSON.stringify(body) });
+    $done({ body: JSON.stringify(body) });
+  } catch (error) {
+    console.error("JSON Parse error:", error);
+  }
+} else {
+  console.error("Response is undefined⚠️警告->>>>>>>>>>>>>>>>");
+}
