fc8b9e2d37efaeca05d174f90171a583871621cc
3kaiu <160321798+3kaiu@users.noreply.github.com>
2024-10-26 02:32:44 +0800

    Update filter8.js

--- qidian/filter8.js
+++ qidian/filter8.js
@@ -4,6 +4,27 @@ if ($response) {
   console.log("filter8" + data);
   try {
     const body = JSON.parse(data);
+    body.Result = 401;
+    body.Message = '已订阅';
+    boday.Data.ChapterCardV2 = {
+      TotalAmount: 100,
+      ABRule: 1,
+      Hint: "可选择章节卡，大额章节卡建议批量订阅使用",
+      TotalCount: 10,
+      CanUseChapterCard: 0,
+      Detail: [
+        {
+          Amount: 100,
+          RuleId: 0,
+          Tip: "2024年11月02日过期",
+          LimitType: 0,
+          Count: 10,
+          Desc: "*全场通用，以章节卡说明为准",
+          DiscountId: "10_0",
+          Name: "10点章节卡",
+        },
+      ],
+    };
     body.Data.CouponPrice = 0;
     body.Data.CouponPrice = 0;
     body.Data.ReadingCouponsPrice = 0;
@@ -19,6 +40,9 @@ if ($response) {
     body.Data.QDBalance = 999;
     body.Data.FreeBalance = 999;
     body.Data.WordBalance = 999;
+    body.Data.PriceInfo.OriginPointPrice = -1;
+    body.Data.PriceInfo.PointDiscountType = -1;
+    body.Data.PriceInfo.ChapterType = 0;
 
     $done({ body: JSON.stringify(body) });
   } catch (error) {
