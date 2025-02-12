30cf416cc0863557cdbcc498437aa2e9b0dd91ba
3kaiu <160321798+3kaiu@users.noreply.github.com>
2025-02-12 10:17:01 +0800

    Update qidian.js

--- qidian/qidian.js
+++ qidian/qidian.js
@@ -1,6 +1,5 @@
 [rewrite local]
-https?:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/adv\/getadvlistbatch?* url reject-200
-# 通用规则，匹配所有需要处理的 URL
+https?:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/adv\/getadvlistbatch url reject-200
 https?:\/\/(rmonitor\.qq\.com|magev6\.if\.qidian\.com|h5\.if\.qidian\.com)\/.* url script-request-body https://github.com/3kaiu/config/raw/main/qidian/universal_script.js
 https?:\/\/(rmonitor\.qq\.com|magev6\.if\.qidian\.com|h5\.if\.qidian\.com)\/.* url script-response-body https://github.com/3kaiu/config/raw/main/qidian/universal_script.js
 
