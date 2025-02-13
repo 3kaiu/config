1380ec3e158fc6e149cc644e5f6ada5b1e84da8b
3kaiu <160321798+3kaiu@users.noreply.github.com>
2025-02-13 09:02:16 +0800

    Update qidian.js

--- qidian/qidian.js
+++ qidian/qidian.js
@@ -2,7 +2,6 @@
 # 精确匹配需要处理的三个API路径
 ^https?:\/\/(rmonitor\.qq\.com|magev6\.if\.qidian\.com)\/argus\/api\/v1\/video\/adv\/(finishWatch|mainPage) url script-request-body https://github.com/3kaiu/config/raw/main/qidian/universal_script.js
 ^https?:\/\/h5\.if\.qidian\.com\/argus\/api\/v3\/user\/getaccountpage url script-response-body https://github.com/3kaiu/config/raw/main/qidian/universal_script.js
-# 广告请求直接拒绝
 ^https?:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/adv\/getadvlistbatch url reject-200
 
 [MITM]
